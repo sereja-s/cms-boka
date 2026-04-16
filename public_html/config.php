553162febc7fa82f1f775ae294c0fe4dd6553207
sereja-s <serejasuvorov1979@gmail.com>
2026-04-16 08:10:18 +0300

    first commit

---
+++ public_html/config.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('VG_ACCESS') or die('Access denied');
+
+// конечный слэш не ставить
+const SITE_URL = 'http://cms-boka';
+const PATH = '/';
+
+const HOST = 'localhost';
+const USER = 'root';
+const PASS = '';
+const DB_NAME = 'cms-boka';
